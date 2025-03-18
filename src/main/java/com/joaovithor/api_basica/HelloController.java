7097ee632a93d619ede897c0a03fee15a367dada
João Vithor <joaovithorbmv@gmail.com>
2025-03-18 20:52:00 -0300

    Commit inicial do projeto

---
+++ src/main/java/com/joaovithor/api_basica/HelloController.java
@@ -0,0 +1,13 @@
+package com.joaovithor.api_basica;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello Spring Boot!";
+    }
+}
